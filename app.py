01a580fcb713e575df271a9d1725df17f5a9f221
JULIUS MWANGI <63066057+JuliusFx131@users.noreply.github.com>
2022-11-20 21:43:10 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('heart_disease_model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -14,14 +14,13 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [float(x) for x in request.form.values()]
+    int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    #final_rf.predict_proba([[41,81,100000]])[0][1]
+    prediction = model.predict_proba(final_features)
 
-    output = round(prediction[0], 2)
+    output = round(prediction[0][1], 4)
 
-    return render_template('index.html', prediction_text='Clients probability of developing a heart disease is {}'.format(output))
+    return render_template('index.html', prediction_text='Clients probability of developing a heart disease is {}{}'.format(output*100,"%"))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
